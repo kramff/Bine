eb0f96b86c448bc284753d70d9902b2062cba43e
Mark Foster <kramff@gmail.com>
2015-10-04 23:58:46 -0400

    batch commit for main bine repository

--- build.bat
+++ build.bat
@@ -7,18 +7,18 @@ cd ../kramff.github.io/
 del /Q *
 echo go back to the source client folder and copy the new files over
 cd ../Bine/Client
-copy * ../../kramff.github.io/
-echo go to the end clinet folder and do git stuff (push to github)
+xcopy * ../../kramff.github.io/
+echo go to the end client folder and do git stuff (push to github)
 cd ../../kramff.github.io/
 git add -A
 git commit -m "batch commit for kramff.github.io" 
 git push
 echo go to server folder and remove app.js
 cd ../bine-online/
-del /Q server.js
+del /Q app.js
 echo go to source server folder and copy new app.js over
 cd ../Bine/Server/
-copy app.js ../../bine-online/
+xcopy app.js ../../bine-online/
 echo go to server folder and do git stuff (push to heroku)
 git add -A
 git commit -m "batch commit for bine-online"
